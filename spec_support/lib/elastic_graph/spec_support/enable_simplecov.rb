d18a70de1dbcddeec32ab90d76df73d70ed7d890
rossroberts-toast <ross.roberts@toasttab.com>
2026-02-13 17:17:12 -0500

    Revert "Explicitly set minimum_coverage to 0 on JRuby"
    This reverts commit 33c4c5094f86f0b660ee127ff9a35866f4ce5f3e.

--- spec_support/lib/elastic_graph/spec_support/enable_simplecov.rb
+++ spec_support/lib/elastic_graph/spec_support/enable_simplecov.rb
@@ -135,10 +135,7 @@ SimpleCov.start do
   # hash literals, etc.) and branch coverage compatibility issues: https://github.com/jruby/jruby/issues/5147
   # We still collect coverage data on JRuby (so it contributes to merged results), but skip enforcement.
   # CRuby (3.4 + 4.0) enforces 100% coverage.
-  if RUBY_PLATFORM == "java"
-    # Explicitly set to 0 to ensure no coverage enforcement on JRuby
-    minimum_coverage line: 0
-  else
+  unless RUBY_PLATFORM == "java"
     enable_coverage :branch
     minimum_coverage line: 100
     minimum_coverage branch: 100
